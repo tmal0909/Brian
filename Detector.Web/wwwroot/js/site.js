7c0d865fb939e5b3992ac0e1389c071676322d3b
mike.chen <tmal0909@gmail.com>
2019-10-15 22:11:17 +0800

    [Web] 加入 wwwroot

---
+++ Detector.Web/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿function init(maxId) {
+    setInterval(function () {
+        GetRecord(maxId);
+    }, 60 * 1000);
+}
+
+function GetRecord(maxId) {
+    $.ajax({
+        url: '/Home/GetRecord',
+        data: { maxId: maxId },
+        success: function (records) {
+            if (records) {
+                alert('接獲入侵通知');
+                window.location.reload();
+            }
+        }
+    });
+}
